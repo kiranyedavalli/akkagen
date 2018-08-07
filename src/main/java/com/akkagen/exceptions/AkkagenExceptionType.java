e2c530b8a3dfdbbe65ef960cd2597a12a7d80d8b
kiran yedavalli <kyedaval@cisco.com>
2018-08-07 12:29:22 -0700

    Updating Copyright

--- src/main/java/com/akkagen/exceptions/AkkagenExceptionType.java
+++ src/main/java/com/akkagen/exceptions/AkkagenExceptionType.java
@@ -1,3 +1,9 @@
+/*
+ * Developed  by Kiran Yedavalli on 8/7/18 12:27 PM
+ * Last Modified 8/6/18 2:33 PM
+ * Copyright (c) 2018. All rights reserved.
+ */
+
 package com.akkagen.exceptions;
 
 public enum AkkagenExceptionType {
--- src/main/java/com/akkagen/serviceproviders/engine/providers/actors/TxRestActor.java
+++ src/main/java/com/akkagen/serviceproviders/engine/providers/actors/TxRestActor.java
@@ -1,3 +1,9 @@
+/*
+ * Developed  by Kiran Yedavalli on 8/7/18 12:27 PM
+ * Last Modified 8/7/18 11:53 AM
+ * Copyright (c) 2018. All rights reserved.
+ */
+
 package com.akkagen.serviceproviders.engine.providers.actors;
 
 import akka.actor.ActorSystem;
